4f530e5983b47ff0e455da551275368bd8120794
Sigmar Ólason <sigmarol266@nemi.tskoli.is>
2019-09-17 12:45:33 +0000

    stff1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    private float speed = 5;
+    public float input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    
+    void FixedUpdate()
+    {
+        // Storing Player Movement
+        input = Input.GetAxisRaw("Horizontal");
+
+
+        // Moving Player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = new Vector2(0, 5);
+        }
+    }
+}
