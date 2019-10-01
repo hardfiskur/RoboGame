b2251d9e90c746a31b808a7bc9c2c5dc3a25528a
sigmarola <42818933+sigmarola@users.noreply.github.com>
2019-10-01 12:38:18 +0000

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector2 dir;
     public Transform planet;
     private float rotForce = 4;
     private Vector3 plDirfromPlayer;
     Rigidbody2D rb;
-
+    float JumpVelocity;
+    float JumpDampening=0.1f;   
     private float speed = 4;
     public float input;
     // Start is called before the first frame update
@@ -24,22 +26,30 @@ public class Player : MonoBehaviour
         transform.right = Vector3.Cross(plDirfromPlayer, Vector3.forward);
         //transform.up = Vector3.Cross(plDirfromPlayer, Vector3.up);
     }
+
     void FixedUpdate()
     {
-
+        rb.velocity = new Vector2(8,rb.velocity.y);
         // Storing Player Movement
         input = Input.GetAxisRaw("Horizontal");
 
-
+        dir = new Vector2(transform.up.x, transform.up.y);
         // Moving Player
         //rb.AddForce (input * transform.right * rotForce);
         //if(input!=0)transform.position =  (input * transform.right * rotForce);
         rb.velocity = input * transform.right * speed;
         //rb.velocity = new Vector2(input * speed, rb.velocity.y);
         if(Input.GetKeyDown(KeyCode.Space)){
-            //transform.GetComponent<Rigidbody2D>().AddForce(plDirfromPlayer*12,ForceMode2D.Impulse);
-            rb.AddForce(transform.up*1500);
-            //rb.velocity = transform.up*10;
+            Jump();
         }
+    }
+    void Jump(){
+        
+        //dir.y-=9.8f*Time.deltaTime;
+        //rb.velocity=dir*55;
+        //rb.velocity = dir*35;
+        //rb.AddForce(dir*3500, ForceMode2D.Force);
+        transform.Translate(0, Time.deltaTime, 0, Space.World);
+        print(dir.y);
     }
 }
