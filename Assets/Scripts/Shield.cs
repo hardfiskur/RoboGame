91c451aa7abd8f65d9f34512d9febf4018c55f34
Sigmar Ólason <sigmarol266@nemi.tskoli.is>
2019-10-17 15:05:35 +0000

    rel

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-    }
+public Transform player;
+Vector3 playerToMouseDir;
+ public float armLength = 0.8f;
+ void Start() {
+     // if the sword is child object, this is the transform of the character (or player)
+     player = transform.parent.transform;
+ }
+ void FixedUpdate() {
+     // Get the direction between the player and mouse (aka the target position)
+     playerToMouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.position;
+     playerToMouseDir.z = 0; 
+     transform.position = player.position + (armLength * playerToMouseDir.normalized);
+ }
 }
